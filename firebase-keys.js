d1ed845d9b36cf562212cd014cb0bbf0dfbc3370
Hexagonews <elliot.loh.2020@gmail.com>
2025-08-23 17:23:07 +0800

    Create firebase-keys.js

---
+++ firebase-keys.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyASQMgjJl2k4kLVZN_0cn8q1TRD7RmHjP8",
+  authDomain: "supersickbike.firebaseapp.com",
+  projectId: "supersickbike",
+  storageBucket: "supersickbike.firebasestorage.app",
+  messagingSenderId: "659436765201",
+  appId: "1:659436765201:web:f518de4363193bf4a1f1b0",
+  measurementId: "G-01RH6YHV8D"
+};
